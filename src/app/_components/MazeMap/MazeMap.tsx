d903cc19bb0d390194f5e1e15446b5e7f85199c1
kristiangjestad <kristiangjestad@icloud.com>
2026-07-23 14:12:12 +0200

    fix(mazemap): disable scroll zoom

--- src/app/_components/MazeMap/MazeMap.tsx
+++ src/app/_components/MazeMap/MazeMap.tsx
@@ -1,29 +1,26 @@
-'use client'
-
 import style from './MazeMap.module.scss'
 
 type PropTypes = {
     height: string
-    campusId?: number,
-    zLevel?: number,
-    center?: {
+    campusId: number,
+    zLevel: number,
+    center: {
         x: number,
         y: number,
     },
-    zoom?: number,
-    sharePoi?: number,
+    zoom: number,
+    sharePoi: number,
 }
+
+type MazeMapLophtetProps = Pick<PropTypes, 'height'>
 
 export default function MazeMap({
     height,
-    campusId = 1, // gløs <3
-    zLevel = -1,
-    center = {
-        x: 10.402228,
-        y: 63.418368,
-    },
-    zoom = 18,
-    sharePoi = 83, // lophtet <3
+    campusId,
+    zLevel,
+    center,
+    zoom,
+    sharePoi,
 }: PropTypes) {
     return <div className={style.MazeMap} style={{ height }}>
         <iframe
@@ -34,12 +31,26 @@ export default function MazeMap({
                 `zlevel=${zLevel}&` +
                 `center=${center.x},${center.y}&` +
                 `zoom=${zoom}&` +
+                'wheelzoom=false&' +
                 'sharepoitype=poi&' +
                 `sharepoi=${sharePoi}&` +
                 'utm_medium=iframe'
-
             }
             className={style.MazeMapIframe}
         />
     </div>
+}
+
+export function MazeMapLophtet({ height }: MazeMapLophtetProps) {
+    return <MazeMap
+        height={height}
+        campusId={1}
+        zLevel={-1}
+        center={{
+            x: 10.402228,
+            y: 63.418368,
+        }}
+        zoom={18}
+        sharePoi={83}
+    />
 }
